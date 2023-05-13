14a850a3cb3608d47bbafc3d54974c70a5697aa2
Ray Haddad <rayhaddad@globalistic.tech>
2023-05-13 11:35:13 +0300

    added support for wine names in bulk predict

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,3 @@
-
-
 from app.utils import WineSample
 from app.services import model
 import pandas as pd
@@ -11,13 +9,16 @@ import re
 
 
 class WineController:
-    def __init__(self,):
+    def __init__(
+        self,
+    ):
         return
-    def wine_predict(self,wine_sample:WineSample):
+
+    def wine_predict(self, wine_sample: WineSample):
         prediction = model.predict(wine_sample.get_dataframe())
         return int(prediction)
 
-    def bulk_wine_predict(self,file):
+    def bulk_wine_predict(self, file):
         df = pd.read_csv(file)
         predictions = []
         # Reading the user csv file
@@ -26,69 +27,97 @@ class WineController:
             # Create a new dataframe for the current row
             current_df = pd.DataFrame(row).T
             # Convert the dataframe to a dictionary
-            current_dictionary = current_df.to_dict(orient='records')[0]
-                # validate user data
-            validated_schema = WineSample(current_dictionary,True)
-            if(validated_schema.validated == False):
-                return {'success':False,'error': validated_schema.errors,'row':i + 1}
-            predictions.append(int(model.predict(current_df)))      
-        return {'predictions':predictions,'success':True,'error': None ,'row':None}  
-    
-    def predict_from_image(self,text):
+            current_dictionary = current_df.to_dict(orient="records")[0]
+            # validate user data
+            validated_schema = WineSample(current_dictionary, True)
+            if validated_schema.validated == False:
+                return {
+                    "success": False,
+                    "error": validated_schema.errors,
+                    "row": i + 1,
+                }
+            predictions.append(
+                {
+                    "name": current_dictionary["name"]
+                    if "name" in current_dictionary
+                    else i + 1,
+                    "prediction": int(
+                        model.predict(
+                            current_df.drop(["name"], axis=1)
+                            if "name" in current_df.columns and False
+                            else current_df
+                        )
+                    ),
+                }
+            )
+        return {"predictions": predictions, "success": True, "error": None, "row": None}
+
+    def predict_from_image(self, text):
         try:
             paragraph = """The wine has a fixed Acidity : 8.2, volatile acidity of 0.57, citric acid of 0.24, residual sugar of 2.1 g/L, chlorides at 0.081 g/L, free sulfur dioxide of 15 mg/L, density of 0.9968 g/mL, pH of 3.29, sulphates at 0.62 g/L, and an alcohol content of 9.9%."""
-            keywords = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
-                        'free sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+            keywords = [
+                "fixed acidity",
+                "volatile acidity",
+                "citric acid",
+                "residual sugar",
+                "chlorides",
+                "free sulfur dioxide",
+                "density",
+                "pH",
+                "sulphates",
+                "alcohol",
+            ]
             values = {}
-            
+
             # matching the keywords
             for keyword in keywords:
-                pattern = r'{}(?:\s*[^0-9]*)([\d.]+)'.format(keyword)
+                pattern = r"{}(?:\s*[^0-9]*)([\d.]+)".format(keyword)
                 match = re.search(pattern, text, re.IGNORECASE)
                 if match:
                     values[keyword] = float(match.group(1))
-            validated_sample = WineSample(values,True)
+            validated_sample = WineSample(values, True)
 
             missing_columns = 0
             # validate values from image
-            if (validated_sample.validated):
+            if validated_sample.validated:
                 # make prediction
                 prediction = model.predict(validated_sample.get_dataframe())
             else:
                 missing_columns = validated_sample.errors.__len__()
             response = {
-                'success':True,
-                'errors':None if validated_sample.validated else validated_sample.errors,
-                'missing_columns':missing_columns,
-                'prediction' :int(prediction) if validated_sample.validated else None,
-                'text':text
+                "success": validated_sample.validated,
+                "errors": None
+                if validated_sample.validated
+                else validated_sample.errors,
+                "missing_columns": missing_columns,
+                "prediction": int(prediction) if validated_sample.validated else None,
+                "text": text,
             }
             return response
         except Exception as e:
             print(e)
             response = {
-                'success':False,
-                'errors':{"Error":"Internal Server Error"},
-                'missing_columns':None,
-                'prediction' :None,
-                'text':None
+                "success": False,
+                "errors": {"Error": "Internal Server Error"},
+                "missing_columns": None,
+                "prediction": None,
+                "text": None,
             }
             return response
-
- 
 
 
 class ImageController:
     def __init__(self):
         return
-    def decode(self,image):
+
+    def decode(self, image):
         image_name = secure_filename(image.filename)
-        image_path = os.path.join('temp', image_name)
+        image_path = os.path.join("temp", image_name)
         image.save(image_path)
 
         img = Image.open(image_path)
         text = pytesseract.image_to_string(img)
-        os.remove(image_path)         
+        os.remove(image_path)
         return text
 
 
--- app/utils.py
+++ app/utils.py
@@ -1,41 +1,42 @@
 import pandas as pd
-from app.models import wine_quality_schema,wine_quality_schema_csv
+from app.models import wine_quality_schema, wine_quality_schema_csv
 from voluptuous import MultipleInvalid
+
 
 # util class to validate wine sample objects
 class WineSample:
-    def __init__(self, data_dict,is_cvs = False):
+    def __init__(self, data_dict, is_csv=False):
         try:
-            if(is_cvs):
+            if is_csv:
                 wine_quality_schema_csv(data_dict)
                 # add values to array for pd
-                converted_dictionary =   {
-                    'fixed acidity': [data_dict['fixed acidity']],
-                    'volatile acidity': [data_dict['volatile acidity']],
-                    'citric acid': [data_dict['citric acid']],
-                    'residual sugar': [data_dict['residual sugar']],
-                    'chlorides': [data_dict['chlorides']],
-                    'free sulfur dioxide': [data_dict['free sulfur dioxide']],
-                    'density': [data_dict['density']],
-                    'pH': [data_dict['pH']],
-                    'sulphates': [data_dict['sulphates']],
-                    'alcohol': [data_dict['alcohol']],
-                }   
+                converted_dictionary = {
+                    "fixed acidity": [data_dict["fixed acidity"]],
+                    "volatile acidity": [data_dict["volatile acidity"]],
+                    "citric acid": [data_dict["citric acid"]],
+                    "residual sugar": [data_dict["residual sugar"]],
+                    "chlorides": [data_dict["chlorides"]],
+                    "free sulfur dioxide": [data_dict["free sulfur dioxide"]],
+                    "density": [data_dict["density"]],
+                    "pH": [data_dict["pH"]],
+                    "sulphates": [data_dict["sulphates"]],
+                    "alcohol": [data_dict["alcohol"]],
+                }
             else:
                 wine_quality_schema(data_dict)
                 # change the dictionary keys to match csv file
-                converted_dictionary =   {
-                    'fixed acidity': [data_dict['fixedAcidity']],
-                    'volatile acidity': [data_dict['volatileAcidity']],
-                    'citric acid': [data_dict['citricAcid']],
-                    'residual sugar': [data_dict['residualSugar']],
-                    'chlorides': [data_dict['chlorides']],
-                    'free sulfur dioxide': [data_dict['freeSulfurDioxide']],
-                    'density': [data_dict['density']],
-                    'pH': [data_dict['pH']],
-                    'sulphates': [data_dict['sulphates']],
-                    'alcohol': [data_dict['alcohol']],
-                }       
+                converted_dictionary = {
+                    "fixed acidity": [data_dict["fixedAcidity"]],
+                    "volatile acidity": [data_dict["volatileAcidity"]],
+                    "citric acid": [data_dict["citricAcid"]],
+                    "residual sugar": [data_dict["residualSugar"]],
+                    "chlorides": [data_dict["chlorides"]],
+                    "free sulfur dioxide": [data_dict["freeSulfurDioxide"]],
+                    "density": [data_dict["density"]],
+                    "pH": [data_dict["pH"]],
+                    "sulphates": [data_dict["sulphates"]],
+                    "alcohol": [data_dict["alcohol"]],
+                }
             self.validated = True
             self.df = pd.DataFrame(converted_dictionary)
         except MultipleInvalid as e:
@@ -46,8 +47,6 @@ class WineSample:
             self.errors = error_messages
             self.validated = False
 
-    # dataframes are used to predict 
+    # dataframes are used to predict
     def get_dataframe(self):
         return self.df
-    
-
